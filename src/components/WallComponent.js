fa5982ab3bac907919b371553ef252a5eed52f38
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-11-07 23:12:29 -0500

    Created empty wall component

---
+++ src/components/WallComponent.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+
+class Wall extends Component {
+    render () {
+        return (
+            <div>
+                <p>show wall here</p>
+            </div>
+        );
+    };
+}
+
+export default Wall;
