4a9b5e7bb582a4655a50a08ccf958ae50e30084d
ferispeakscode@gmail.com <ferispeakscode@gmail.com>
2020-11-07 23:25:46 -0500

    Show wall image via img tag

--- src/components/WallComponent.js
+++ src/components/WallComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import '../styles/wall.css';
+import wall from '../images/wood.jpg';
 
 class Wall extends Component {
     render () {
         return (
-            <div className="wall"></div>
+            <div className="wall">
+                <img src={wall}></img>
+            </div>
         );
     };
 }
